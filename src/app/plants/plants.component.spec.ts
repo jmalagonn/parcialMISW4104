cdf2bd5688332fabbeaa8588858e378dd6c9125d
jmalagonnavarro <juan.malagonnavarro@amadeus.com>
2024-04-26 09:14:16 -0500

    Tests implemented

--- src/app/plants/plants.component.spec.ts
+++ src/app/plants/plants.component.spec.ts
@@ -1,23 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlantsComponent } from './plants.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Plant } from '../models/plant';
+import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
+import { By } from '@angular/platform-browser';
 
 describe('PlantsComponent', () => {
   let component: PlantsComponent;
   let fixture: ComponentFixture<PlantsComponent>;
+  let debug: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [PlantsComponent]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(PlantsComponent);
     component = fixture.componentInstance;
+
+    const plants: Plant[] = [];
+
+    for (let i = 0; i < 3; i++) {
+      plants.push({
+        id: faker.number.int(),
+        nombre_cientifico: faker.lorem.sentence(),
+        nombre_comun: faker.lorem.sentence(),
+        tipo: faker.number.int() % 2 === 0 ? 'Interior' : 'Exterior',
+        altura_maxima: faker.number.int(),
+        sustrato_siembra: faker.lorem.sentence(),
+        clima: faker.lorem.sentence(),
+      });
+    }
+    
+    component.plants = plants;
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should contain 3 plants', () => {
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4);
   });
 });
--- src/app/services/plants.service.spec.ts
+++ src/app/services/plants.service.spec.ts
@@ -1,16 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { PlantsService } from './plants.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('PlantsService', () => {
   let service: PlantsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PlantsService]
+    });
     service = TestBed.inject(PlantsService);
   });
 
-  it('should be created', () => {
+  it('should ...', inject([PlantsService], (service: PlantsService) => {
     expect(service).toBeTruthy();
-  });
+  }));
 });
